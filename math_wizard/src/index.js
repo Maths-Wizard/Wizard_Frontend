368f4e4d32905b687e5d5d470e7db8288622e907
ASJ18 <96025983+ASJ18@users.noreply.github.com>
2023-07-02 10:15:34 +0100

    little adjustment made to app.js

--- math_wizard/src/index.js
+++ math_wizard/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import { ContextProvider } from './contexts/ContextProvider';
+// import { ContextProvider } from './contexts/ContextProvider';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
-  );
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ContextProvider>
+//       <App />
+//     </ContextProvider>
+//   </React.StrictMode>,
+  ReactDOM.render(<App />, document.getElementById('root'));
